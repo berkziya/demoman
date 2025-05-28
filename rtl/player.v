c15c5ba1d33623770104147140540304dcf25cc3
berkziya <berkziya@proton.me>
2025-05-28 16:09:52 +0300

    some

--- rtl/player.v
+++ rtl/player.v
@@ -6,7 +6,7 @@ module player #(
   input            left, right, attack,
   output reg [9:0] posx,
   output reg [9:0] posy,
-  output [3:0] currentstate
+  output     [3:0] currentstate
 );
 parameter LEFT = 1'b0;
 parameter RIGHT = 1'b1;
@@ -18,11 +18,12 @@ parameter S_B_ATTACK_START = 3'd3;
 parameter S_B_ATTACK_END = 3'd4;
 parameter S_B_ATTACK_PULL = 3'd5;
 
+parameter P_SPEED = 1;
+
 reg [2:0] CS, NS;
 
 wire [10:0] counter;
-
-reg        rst_counter;
+reg rst_counter;
 
 counter #(
   .W(5)
@@ -32,8 +33,6 @@ counter #(
   .control(2'b01),
   .count(counter)
 );
-
-parameter P_SPEED = 1;
 
 always @(posedge clk or posedge rst) begin
   if (rst) CS <= S_IDLE;
@@ -52,30 +51,31 @@ always @(*) begin
       end else NS = S_IDLE;
     end
     S_B_ATTACK_START: begin
-	  rst_counter = 1'b0;
-      if (counter < 4) NS = S_B_ATTACK_START;
-      else 
-		begin
-			rst_counter = 1'b1;
-			NS = S_B_ATTACK_END;
-		end
+      rst_counter = 1'b0;
+        if (counter < 4) NS = S_B_ATTACK_START;
+        else
+      begin
+        rst_counter = 1'b1;
+        NS = S_B_ATTACK_END;
+      end
     end
     S_B_ATTACK_END: begin
-      if (counter < 1) NS = S_B_ATTACK_END;
-      else 
-		begin
-			rst_counter = 1'b1;
-			NS = S_B_ATTACK_PULL;
-		end
+      rst_counter = 1'b0;
+        if (counter < 1) NS = S_B_ATTACK_END;
+        else
+      begin
+        rst_counter = 1'b1;
+        NS = S_B_ATTACK_PULL;
+      end
     end
     S_B_ATTACK_PULL: begin
-	  rst_counter = 1'b0;
-      if (counter < 15) NS = S_B_ATTACK_PULL;
-      else 
-		begin
-			rst_counter = 1'b1;
-			NS = S_IDLE;
-		end
+      rst_counter = 1'b0;
+        if (counter < 15) NS = S_B_ATTACK_PULL;
+        else
+      begin
+        rst_counter = 1'b1;
+        NS = S_IDLE;
+      end
     end
     default: NS = S_IDLE;
   endcase
@@ -83,22 +83,20 @@ end
 
 always @(*) begin
   if (rst) begin
-    posx <= SIDE == LEFT ? 10'd210 : 10'd420;
-    posy <= 10'd240;
+    posx <= (SIDE == LEFT) ? 10'd210 : 10'd420;
+    posy <= 10'd140;
   end else begin
     case (CS)
       S_IDLE: begin
         posx <= posx;
       end
       S_MOVEFORWARD: begin
-        if (SIDE == LEFT) posx <= posx - P_SPEED;
-        else posx <= posx + P_SPEED;
-
-      end
-      S_MOVEBACKWARDS: begin
         if (SIDE == LEFT) posx <= posx + P_SPEED;
         else posx <= posx - P_SPEED;
-
+      end
+      S_MOVEBACKWARDS: begin
+        if (SIDE == LEFT) posx <= posx - P_SPEED;
+        else posx <= posx + P_SPEED;
       end
       S_B_ATTACK_START: begin
         posx <= posx;
@@ -115,7 +113,7 @@ always @(*) begin
       end
     endcase
   end
-  if (posx < 50) posx <= 0;
+  if (posx < 50) posx <= 50;
   else if (posx > 590) posx <= 590;
 end
 
---
+++ tools/pngto8bithex.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from PIL import Image
+import os
+import sys
+
+
+def rgb_to_rrrgggbb(r, g, b):
+    """Convert 8-bit RGB to 3-3-2 RRRGGGBB format."""
+    r3 = (r >> 5) & 0x07
+    g3 = (g >> 5) & 0x07
+    b2 = (b >> 6) & 0x03
+    return (r3 << 5) | (g3 << 2) | b2
+
+
+def image_to_hex(image_path, output_path):
+    img = Image.open(image_path).convert("RGB")
+    pixels = list(img.getdata())
+
+    with open(output_path, "w") as f:
+        for r, g, b in pixels:
+            hex_val = rgb_to_rrrgggbb(r, g, b)
+            f.write(f"{hex_val:02X}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        input_path = sys.argv[1]
+        output_path = os.path.splitext(input_path)[0] + ".hex"
+        image_to_hex(input_path, output_path)

9d6d0318758362372bdd89ed02a55a4c5af10fae
Feyza Nur Söğüt <feyza.nur.sogut00@gmail.com>
2025-06-04 23:38:10 +0300

    A

--- rtl/utils/random_num.v
+++ rtl/utils/random_num.v
@@ -4,7 +4,7 @@ module random_num #(
   input              clk,
   output reg [W-1:0] rand_o
 );
-  reg [W-1:0] seed = 32'hDEADBEEF; // Initial seed value, can be any non-zero value
+  reg [W-1:0] seed = 32'hEEE12345; // Initial seed value, can be any non-zero value
 
   always @(posedge clk) begin
     seed   <= {seed[W-2:0], seed[W-1] ^ seed[W-2] ^ seed[W-3] ^ seed[W-4]};
